bac9a1e7ffd6fe55ed9bc4f77d65a45140845bdd
Caio Assis <caio.ab.assis@gmail.com>
2025-05-11 09:41:56 -0300

    Altera CRUD de User (cliente)

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -6,195 +6,88 @@ export default class UserController {
   static async getUserList(req: Request, res: Response) {
     try {
       const database = await connectToDatabase()
-      const collection = database.collection('Users')
+      const collection = database.collection('User')
       const users = await collection.find({}).toArray()
       res.status(200).json(users)
     } catch (error) {
-      console.error('Erro ao buscar dados da coleção pedido', error)
-      res.status(500).json({ error: 'Erro ao buscar dados da coleção pedido' })
+      console.error('Erro ao buscar usuários:', error)
+      res.status(500).json({ error: 'Erro ao buscar usuários' })
     }
   }
-  ///////// SÓ FUNCIONA CONTROLLER GETUSERLIST. RESTO AINDA NAO ARRUMADO
-  static async getOrderById(req: Request, res: Response) {
+
+  static async getUserById(req: Request, res: Response) {
     try {
       const { id } = req.params
-      const database = await connectToDatabase();
-      const collection = database.collection('Pedido')
-      const pedido = await collection.findOne({ _id: new ObjectId(id) })
-      if (pedido) {
-        res.status(200).json(pedido)
+      const database = await connectToDatabase()
+      const collection = database.collection('User')
+      const user = await collection.findOne({ _id: new ObjectId(id) })
+
+      if (user) {
+        res.status(200).json(user)
       } else {
-        res.status(404).json({ error: "Pedido não encontrado." })
+        res.status(404).json({ error: 'Usuário não encontrado.' })
       }
     } catch (error) {
-      console.error('Erro ao buscar dados na coleção Pedidos:', error)
-      res.status(500).json({ error: 'Erro ao buscar dados na coleção Pedidos' })
+      console.error('Erro ao buscar usuário:', error)
+      res.status(500).json({ error: 'Erro ao buscar usuário' })
     }
   }
-  
-  static async getNoDoneOrders(req: Request, res: Response) {
+
+  static async createUser(req: Request, res: Response) {
     try {
+      const { uid, name, email, cpf } = req.body
+      const newUser = { uid, name, email, cpf }
+
       const database = await connectToDatabase()
-      const collection = database.collection('Pedido')
-      const pedidos = await collection.find({entregue: false}).sort({data_pedido: 1}).toArray()
-      res.status(200).json(pedidos)
-    } catch (error) {
-      console.error('Erro ao buscar pedidos pendentes:', error)
-      res.status(500).json({ error: 'Erro ao buscar pedidos pendentes' })
-    }
-  }
-  
-  static async getUnpaidOrders(req: Request, res: Response) {
-    try {
-      const database = await connectToDatabase()
-      const collection = database.collection('Pedido')
-      const {cod_comanda} = req.params
-      const pedidos = await collection.find({pago: false, cod_comanda: cod_comanda}).toArray()
+      const collection = database.collection('User')
+      const result = await collection.insertOne(newUser)
 
-      res.status(200).json(pedidos)
+      res.status(201).json({ _id: result.insertedId, ...newUser })
     } catch (error) {
-      console.error('Erro ao buscar pedidos não pagos por comandas:', error)
-      res.status(500).json({ error: 'Erro ao buscar pedidos não pagos por comandas' })
+      console.error('Erro ao criar usuário:', error)
+      res.status(500).json({ error: 'Erro ao criar usuário' })
     }
   }
 
-  static async createOrder(req: Request, res: Response) {
-    try {
-      const { cod_mesa, cod_comanda, produtos, data_pedido, entregue, pago, total } = req.body
-      const newOrder = {
-        cod_mesa,
-        cod_comanda,
-        produtos,
-        data_pedido,
-        entregue,
-        pago,
-        total,
-      }
-
-      const database = await connectToDatabase();
-      const collection = database.collection('Pedido')
-      const result = await collection.insertOne(newOrder)
-
-      res.status(201).json({ _id: result.insertedId, ...newOrder })
-    } catch (error) {
-      console.error('Erro ao criar novo pedido.', error)
-      res.status(500).json({ error: 'Erro ao criar novo pedido.' })
-    }
-  }
-
-  static async updateOrder(req: Request, res: Response) {
+  static async updateUser(req: Request, res: Response) {
     try {
       const { id } = req.params
-      const { cod_mesa, cod_comanda, produtos, data_pedido, entregue, pago, total } = req.body
-      const updatedData = {
-        cod_mesa,
-        cod_comanda,
-        produtos,
-        data_pedido,
-        entregue,
-        pago,
-        total,
-      }
+      const { uid, name, email, cpf } = req.body
+      const updatedUser = { uid, name, email, cpf }
 
-      const database = await connectToDatabase();
-      const collection = database.collection('Pedido')
+      const database = await connectToDatabase()
+      const collection = database.collection('User')
       const result = await collection.updateOne(
         { _id: new ObjectId(id) },
-        { $set: updatedData }
+        { $set: updatedUser }
       )
 
       if (result.matchedCount > 0) {
-        res.status(200).json({ message: 'Pedido atualizado com sucesso' })
+        res.status(200).json({ message: 'Usuário atualizado com sucesso' })
       } else {
-        res.status(400).json({ error: 'Pedido não encontrado' })
+        res.status(404).json({ error: 'Usuário não encontrado' })
       }
     } catch (error) {
-      console.error('Erro ao atualizar pedido:', error)
-      res.status(500).json({ error: 'Erro ao atializar pedido' })
+      console.error('Erro ao atualizar usuário:', error)
+      res.status(500).json({ error: 'Erro ao atualizar usuário' })
     }
   }
 
-  static async removeOrder(req: Request, res: Response) {
+  static async removeUser(req: Request, res: Response) {
     try {
       const { id } = req.params
       const database = await connectToDatabase()
-      const collection = database.collection('Pedido')
+      const collection = database.collection('User')
       const result = await collection.deleteOne({ _id: new ObjectId(id) })
 
       if (result.deletedCount > 0) {
-        res.status(200).json({ message: 'Pedido removido com successo.' })
+        res.status(200).json({ message: 'Usuário removido com sucesso' })
       } else {
-        res.status(404).json({ error: 'Pedido não encontrado.' })
+        res.status(404).json({ error: 'Usuário não encontrado' })
       }
     } catch (error) {
-      console.error('Erro ao remover pedido: ', error)
-      res.status(500).json({ error: 'Erro ao remover pedido' })
-    }
-  }
-
-  static async getOrderByData(req: Request, res: Response) {
-    try {
-      const database = await connectToDatabase()
-      const collection = database.collection('Pedido')
-      const {data_pedido} = req.params
-      const dayStart = new Date(`${data_pedido}T00:00:00.000Z`)
-      const dayEnd = new Date(`${data_pedido}T23:59:59.999Z`)
-
-      //console.log(data_pedido)
-      const pedidos = await collection.find(
-        {data_pedido: 
-          {
-            $gte: dayStart,
-            $lte: dayEnd
-          }
-        }).toArray()
-
-      res.status(200).json(pedidos)
-    } catch (error) {
-      console.error('Erro ao buscar pedidos não pagos por comandas:', error)
-      res.status(500).json({ error: 'Erro ao buscar pedidos não pagos por comandas' })
-    }
-  }
-
-  static async getOrderByCommand(req: Request, res: Response) {
-    try {
-      const database = await connectToDatabase()
-      const collection = database.collection('Pedido')
-      const {cod_comanda} = req.params
-
-      //console.log(cod_comanda)
-      const pedidos = await collection.find({ pago: false, cod_comanda: Number(cod_comanda) }).toArray()
-
-      res.status(200).json(pedidos)
-    } catch (error) {
-      console.error('Erro ao buscar pedidos não pagos por comandas:', error)
-      res.status(500).json({ error: 'Erro ao buscar pedidos não pagos por comandas' })
-    }
-  }
-
-  static async getOrderByDataAndCommand(req: Request, res: Response) {
-    try {
-      const database = await connectToDatabase()
-      const collection = database.collection('Pedido')
-      const {data_pedido, cod_comanda} = req.params
-      const dayStart = new Date(`${data_pedido}T00:00:00.000Z`)
-      const dayEnd = new Date(`${data_pedido}T23:59:59.999Z`)
-
-      //console.log(cod_comanda)
-      //console.log(data_pedido)
-      const pedidos = await collection.find(
-        { cod_comanda: Number(cod_comanda),
-          data_pedido: 
-          {
-            $gte: dayStart,
-            $lte: dayEnd
-          }
-        }).toArray()
-
-      res.status(200).json(pedidos)
-    } catch (error) {
-      console.error('Erro ao buscar pedidos não pagos por comandas:', error)
-      res.status(500).json({ error: 'Erro ao buscar pedidos não pagos por comandas' })
+      console.error('Erro ao remover usuário:', error)
+      res.status(500).json({ error: 'Erro ao remover usuário' })
     }
   }
 }
--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -3,6 +3,13 @@ import UserController from '../controllers/user-controller'
 
 const userRoutes = Router()
 
-userRoutes.get('/userlist', UserController.getUserList)
+userRoutes.post('/new', UserController.createUser)
+
+userRoutes.get('/users', UserController.getUserList)
+userRoutes.get('/users/:id', UserController.getUserById)
+
+userRoutes.put('/users/:id', UserController.updateUser)
+
+userRoutes.delete('/users/:id', UserController.removeUser)
 
 export default userRoutes
